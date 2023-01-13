c1674ca5725db5764dc042b1f8ff9542a3264ec6
Holewalker <dani75esp@gmail.com>
2023-01-13 13:51:48 +0100

    Stopwatch task + Guava dependency

---
+++ src/main/java/com/svalero/relojdigitalpsp/task/StopwatchTask.java
@@ -0,0 +1,17 @@
+package com.svalero.relojdigitalpsp.task;
+
+import com.google.common.base.Stopwatch;
+import javafx.concurrent.Task;
+
+public class StopwatchTask extends Task<Integer> {
+    @Override
+    protected Integer call() throws Exception {
+        String elapsed;
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        elapsed= String.valueOf(stopwatch.elapsed());
+        //TODO A come
+
+
+        return null;
+    }
+}
